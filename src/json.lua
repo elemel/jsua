49743b7af4ad16760bcdef3fc6231b17996313a2
Mikael Lind <elemel@elemel-laptop.(none)>
2008-11-30 10:06:29 +0100

    Partial rewrite of the json module. Renamed json.decode() to parse() and json.encode() to format().

--- src/json.lua
+++ src/json.lua
@@ -21,171 +21,222 @@
 -- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 -- OTHER DEALINGS IN THE SOFTWARE.
 
-local json = {}
+local json = {
+}
 
 json.Object = {}
 json.Array = {}
+json.null = {}
 
 local impl = {}
 
 local escapes = {["\""] = "\"", ["\\"] = "\\", ["/"] = "/", b = "\b",
                  f = "\f", n = "\n", r = "\r", t = "\t"}
 
-local number_chars = "-0123456789.eE"
-local whitespace_chars = " \n\r\t"
-
-function is_substring(sub, str)
-    return string.find(str, sub, 1, true) ~= nil
-end
-
 function impl.is_number_char(char)
-    return is_substring(char, number_chars)
+    return string.find("-0123456789.eE", char, 1, true) ~= nil
 end
 
 function impl.is_whitespace_char(char)
-    return is_substring(char, whitespace_chars)
+    return string.find(" \n\r\t", char, 1, true) ~= nil
 end
 
-function impl.decode(str, pos)
-    local char = string.sub(str, pos, pos)
+function impl.parse(peek_char, read_char)
+    impl.skip_whitespace(peek_char, read_char)
+    local value = impl.parse_value(peek_char, read_char)
+    impl.skip_whitespace(peek_char, read_char)
+    assert(not peek_char())
+    return value
+end
+
+function impl.parse_value(peek_char, read_char)
+    local char = peek_char()
     if char == "\"" then
-        return impl.decode_string(str, pos)
+        return impl.parse_string(peek_char, read_char)
     elseif impl.is_number_char(char) then
-        return impl.decode_number(str, pos)
+        return impl.parse_number(peek_char, read_char)
     elseif char == "{" then
-        return impl.decode_object(str, pos)
+        return impl.parse_object(peek_char, read_char)
     elseif char == "[" then
-        return impl.decode_array(str, pos)
+        return impl.parse_array(peek_char, read_char)
     elseif char == "t" then
-        return impl.decode_value(str, pos, "true", true)
+        return impl.parse_constant(peek_char, read_char, "true", true)
     elseif char == "f" then
-        return impl.decode_value(str, pos, "false", false)
+        return impl.parse_constant(peek_char, read_char, "false", false)
     elseif char == "n" then
-        return impl.decode_value(str, pos, "null", json.null)
+        return impl.parse_constant(peek_char, read_char, "null", json.null)
     else
-        impl.decode_error(str, pos)
+        assert(false)
     end
 end
 
-function impl.skip_whitespace(str, pos)
-    local len = #str
-    while pos <= len do
-        local char = string.sub(str, pos, pos)
-        if char == "/" then
-            pos = impl.skip_comment(str, pos)
+function impl.skip_whitespace(peek_char, read_char)
+    while true do
+        local char = peek_char()
+        if not char then
+            break
+        elseif char == "/" then
+            impl.skip_comment(peek_char, read_char)
         elseif impl.is_whitespace_char(char) then
-            pos = pos + 1
+            read_char()
         else
             break
         end
     end
-    return pos
 end
 
-function impl.skip_comment(str, pos)
-    local char = string.sub(str, pos + 1, pos + 1)
+function impl.skip_comment(peek_char, read_char)
+    local char = read_char()
+    assert(char == "/")
+    char = read_char()
     if char == "/" then
-        return (string.find(str, "\n", pos + 2, true) or #str) + 1
+        repeat
+            char = read_char()
+        until not char or char == "\n"
+        return nil
     elseif char == "*" then
-        return string.find(str, "*/", pos + 2, true) + 2
+        local star = false
+        while true do
+            char = read_char()
+            assert(char)
+            if char == "*" then
+                star = true
+            elseif star and char == "/" then
+                return nil
+            else
+                star = false
+            end
+        end
     else
-        impl.decode_error(str, pos)
+        assert(false)
     end
 end
 
-function impl.decode_string(str, pos)
+function impl.parse_string(peek_char, read_char)
     local buffer = {}
+    local char = read_char()
+    assert(char == "\"")
     while true do
-        pos = pos + 1
-        local char = string.sub(str, pos, pos)
-        if char == "" then
-            error("EOF in JSON string")
-        elseif char == "\"" then
+        char = read_char()
+        assert(char)
+        if char == "\"" then
             break
         elseif char == "\\" then
-            pos = pos + 1
-            char = string.sub(str, pos, pos)
+            char = read_char()
             if escapes[char] then
                 table.insert(buffer, escapes[char])
             elseif char == "u" then
-                error("JSON unicode escapes are not supported")
+                -- JSON unicode escapes are not supported.
+                assert(false)
             else
-                impl.decode_error(str, pos)
+                assert(false)
             end
         else
             table.insert(buffer, char)
         end
     end
-    return table.concat(buffer, ""), pos + 1
+    return table.concat(buffer)
 end
 
-function impl.decode_number(str, pos)
-    local stop = pos + 1
-    while stop <= #str and impl.is_number_char(string.sub(str, stop, stop))  do
-        stop = stop + 1
+function impl.parse_number(peek_char, read_char)
+    local buffer = {}
+    while peek_char() and impl.is_number_char(peek_char()) do
+        table.insert(buffer, read_char())
     end
-    return tonumber(string.sub(str, pos, stop - 1)), stop
+    return tonumber(table.concat(buffer))
 end
 
-function impl.decode_object(str, pos)
+function impl.parse_object(peek_char, read_char)
     local obj = {}
-    return obj, pos + 2
-end
-
-function impl.decode_array(str, pos)
-    local arr = {}
-    return arr, pos + 2
-end
-
-function impl.decode_value(str, pos, pattern, value)
-    if string.sub(str, pos, pos + #pattern - 1) ~= pattern then
-        impl.decode_error(str, pos)
+    setmetatable(obj, json.Object)
+    local char = read_char()
+    assert(char == "{")
+    while true do
+        impl.skip_whitespace(peek_char, read_char)
+        char = peek_char()
+        assert(char)
+        if char == "}" then
+            read_char()
+            break
+        end
+        local name = impl.parse_value(peek_char, read_char)
+        impl.skip_whitespace(peek_char, read_char)
+        local char = read_char()
+        assert(char == ":")
+        impl.skip_whitespace(peek_char, read_char)
+        local value = impl.parse_value(peek_char, read_char)
+        obj[name] = value
+        char = read_char()
+        if char == "}" then
+            break
+        end
+        assert(char == ",")
     end
-    return value, pos + #pattern
+    return obj
 end
 
-function impl.decode_error(str, pos)
-    error("JSON decode error at position " .. pos)
-end
-
-function json.array(arr)
-    arr = arr or {}
-    arr[json.tag] = json.array
+function impl.parse_array(peek_char, read_char)
+    local arr = {}
+    setmetatable(arr, json.Array)
+    local char = read_char()
+    assert(char == "[")
+    while true do
+        impl.skip_whitespace(peek_char, read_char)
+        char = peek_char()
+        assert(char)
+        if char == "]" then
+            read_char()
+            break
+        end
+        table.insert(arr, impl.parse_value(peek_char, read_char))
+        impl.skip_whitespace(peek_char, read_char)
+        char = read_char()
+        assert(char)
+        if char == "]" then
+            break
+        end
+        assert(char == ",")
+    end
     return arr
 end
 
-function json.decode(str)
-    local pos = impl.skip_whitespace(str, 1)
-    local value, pos = impl.decode(str, pos)
-    local pos = impl.skip_whitespace(str, pos)
-    if pos <= #str then
-        impl.decode_error(str, pos)
+function impl.parse_constant(peek_char, read_char, name, value)
+    local i = 1
+    while i <= #name do
+        local char = read_char()
+        assert(char == string.sub(name, i, i))
+        i = i + 1
     end
     return value
 end
 
-function json.encode(val)
-    local buffer = {}
-    impl.encode(val, buffer)
-    return table.concat(buffer, "")
-end
-
-function json.null()
-    return json.null
-end
-
-function json.object(obj)
-    obj = obj or {}
-    obj[json.tag] = json.object
-    return obj
-end
-
-function json.tag(val)
-    if type(val) == "table" then
-        return val[json.tag]
-    else
-        return nil
+function json.parse(str)
+    local pos = 1
+    local function peek_char()
+        return pos <= #str and string.sub(str, pos, pos) or nil
     end
+    local function read_char()
+        local char = peek_char()
+        pos = pos + 1
+        return char
+    end
+    if true then
+        return impl.parse(peek_char, read_char)
+    end
+    local success, result = pcall(impl.parse, peek_char, read_char)
+    if not success then
+        error("JSON parse error at character " .. pos)
+    end
+    return result
+end
+
+function json.format(val)
+    local buffer = {}
+    local function write_char(char)
+        table.insert(buffer, char)
+    end
+    impl.format_value(val, write_char)
+    return table.concat(buffer)
 end
 
 return json
--- src/json_test.lua
+++ src/json_test.lua
@@ -1,70 +1,60 @@
 local json = require("json")
 
-local function test_array()
-    local arr = json.array()
+local function test_parse_array()
+    local arr = json.parse("[]")
     assert(type(arr) == "table")
-    assert(arr[json.tag] == json.array)
-
-    local arr_2 = {1, 3, 8}
-    local arr_3 = json.array(arr_2)
-    assert(arr_2 == arr_3)
-    assert(arr_2[json.tag] == json.array)
-end
-
-local function test_decode_string()
-    assert(json.decode("\"\"") == "")
-    assert(json.decode("\"The Dude abides.\"") == "The Dude abides.")
-    assert(json.decode("\"\\\"\\\\\\/\"") == "\"\\/")
-    assert(json.decode("\"\\b\\f\\n\\r\\t\"") == "\b\f\n\r\t")
-end
-
-local function test_decode_number()
-    assert(json.decode("0") == 0)
-    assert(json.decode("42") == 42)
-end
-
-local function test_decode_object()
-end
-
-local function test_decode_array()
-    local arr = json.decode("[]")
-    assert(type(arr) == "table")
+    assert(json.Array and getmetatable(arr) == json.Array)
     assert(#arr == 0)
+
+    local arr = json.parse("[2,3,5]")
+    assert(#arr == 3)
+    assert(arr[1] == 2)
+    assert(arr[2] == 3)
+    assert(arr[3] == 5)
 end
 
-local function test_decode_true()
-    assert(json.decode("true") == true)
+local function test_parse_string()
+    assert(json.parse("\"\"") == "")
+    assert(json.parse("\"The Dude abides.\"") == "The Dude abides.")
+    assert(json.parse("\"\\\"\\\\\\/\"") == "\"\\/")
+    assert(json.parse("\"\\b\\f\\n\\r\\t\"") == "\b\f\n\r\t")
 end
 
-local function test_decode_false()
-    assert(json.decode("false") == false)
+local function test_parse_number()
+    assert(json.parse("0") == 0)
+    assert(json.parse("42") == 42)
 end
 
-local function test_decode_null()
-    assert(json.decode("null") == json.null)
-end
-
-local function test_null()
-    assert(json.null() == json.null)
-end
-
-local function test_object()
-    local obj = json.object()
+local function test_parse_object()
+    local obj = json.parse("{}")
     assert(type(obj) == "table")
-    assert(obj[json.tag] == json.object)
+    assert(json.Object and getmetatable(obj) == json.Object)
+    assert(#obj == 0)
+    
+    local obj = json.parse('{"the":true,"dude":false,"abides":null}')
+    assert(obj.the == true)
+    assert(obj.dude == false)
+    assert(obj.abides == json.null)
+end
 
-    local obj_2 = {the = 1, dude = 3, abides = 8}
-    local obj_3 = json.object(obj_2)
-    assert(obj_2 == obj_3)
-    assert(obj_2[json.tag] == json.object)
+local function test_parse_true()
+    assert(json.parse("true") == true)
+end
+
+local function test_parse_false()
+    assert(json.parse("false") == false)
+end
+
+local function test_parse_null()
+    assert(json.null and json.parse("null") == json.null)
 end
 
 local function test_skip_comment()
-    assert(json.decode("13 // thirteen") == 13)
-    assert(json.decode("// thirteen\n13") == 13)
+    assert(json.parse("13 // thirteen") == 13)
+    assert(json.parse("// thirteen\n13") == 13)
 
-    assert(json.decode("/* forty */ 42 /* two */") == 42)
-    assert(json.decode([[
+    assert(json.parse("/* forty */ 42 /* two */") == 42)
+    assert(json.parse([[
                          /*
                           * forty-two
                           */
@@ -73,16 +63,13 @@ local function test_skip_comment()
 end
 
 local function test()
-    test_array()
-    test_decode_array()
-    test_decode_false()
-    test_decode_null()
-    test_decode_number()
-    test_decode_object()
-    test_decode_string()
-    test_decode_true()
-    test_null()
-    test_object()
+    test_parse_array()
+    test_parse_false()
+    test_parse_null()
+    test_parse_number()
+    test_parse_object()
+    test_parse_string()
+    test_parse_true()
     test_skip_comment()
 end
 
